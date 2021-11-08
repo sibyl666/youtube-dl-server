74df5d4d0a4ec8cf1794a2376a0196c15c1e3b7a
Sibyl666 <metinkorkmaz417@gmail.com>
2021-11-08 12:53:23 +0300

    return thumbnail data too

--- src/main.ts
+++ src/main.ts
@@ -30,65 +30,37 @@ app.get("/", (req, res) => {
   const child = spawn(`yt-dlp`, [
     `${queryUrl}`,
     "--no-simulate",
-    "--write-thumbnail",
     "--no-part",
+    "--write-thumbnail",
+    "--write-info-json",
     `-P ${__dirname}/videos`,
     "--output=%(id)s.%(ext)s",
     "--print=%(id)s.%(ext)s"
   ]);
   child.stdout.on("data", chunk => {
     let chunkString = chunk.toString() as string;
-    console.log(chunkString)
-    let match = videoRegex.exec(chunkString);
+
+    const match = videoRegex.exec(chunkString);
     if (!match) return;
 
-    res.send({
-      fileUri: match[0]
+    readFile(`${__dirname}/videos/${match[1]}.info.json`, (err, data) => {
+      if (err) {
+        res.status(500).send("Can't read info json");
+        return;
+      }
+
+      const content = JSON.parse(data.toString());
+      res.send({
+        fileUri: match[0],
+        thumbnail: content.thumbnails[content.thumbnails.length - 1]
+      })
     })
   })
   child.stderr.on("error", err => {
     console.log("eror", err)
   })
 
-  child.on("close", code => console.log("code", code))
-  
-  // exec(`yt-dlp "${queryUrl}" -P "${__dirname}/videos" -o "%(id)s.%(ext)s" -r 10K --write-thumbnail --write-info-json --xattr-set-filesize`,
-  // async (err, stdout) => {
-  //   console.log(stdout);
-  // });
-
-  // exec(`yt-dlp "${queryUrl}" -P "${__dirname}/videos" -o "%(id)s.%(ext)s" \
-  //   --write-thumbnail \
-  //   --write-info-json
-  // `,
-  // async (err, stdout) => {
-  //   if (err) {
-  //     res.status(500).send(err);
-  //     return;
-  //   }
-
-  //   const match  = getIdFromStdout(stdout);
-  //   if (!match) {
-  //     res.status(500).send("Can't get ID from the downloaded local file!");
-  //     return;
-  //   }
-
-  //   // match[0] FILE FULL NAME WITH EXTENSION
-    
-
-  //   readFile(`${__dirname}/videos/${match[1]}.info.json`, (err, data) => {
-  //     if (err) {
-  //       res.status(500).send("Can't read local json file!");
-  //       return;
-  //     }
-
-  //     const content = JSON.parse(data.toString());
-  //     res.send({
-  //       fileUri: match[0],
-  //       thumbnail: content.thumbnails[content.thumbnails.length - 1]
-  //     })
-  //   })
-  // })
+  child.on("close", code => console.log("code", code));
 })
 
 app.listen(8000, () => console.log("listening on port 8000!"));
